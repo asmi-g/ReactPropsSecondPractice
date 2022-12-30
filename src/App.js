4f5bdd45dfacc155dfd3eb3f0e6532fb07e510d4
asmi-g <108370938+asmi-g@users.noreply.github.com>
2022-12-30 03:20:09 -0500

    second-commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
 import Joke from "./Joke"
+import jokesData from './jokesData';
+
+/* Change made: Instead of hard coding each joke's properties into an individual JSX component call of <Joke/>, 
+a file called jokesData.js was created to store an array of objects, with one property of question(joke setup), and one property of answer(joke punchline).
+Then, the array.map() method was used to access each individual item in the array, and equate the value of their question, 
+to the setup property in the JSX component of <Joke/>, and the same was done for the answer with the punchline property. What was returned was the jokeElements array,
+which stored the jokes that print to the screen */
 
 function App() {
-  return (
+  const jokeElements = jokesData.map(joke => {
+    return <Joke setup={joke.question} punchline={joke.answer}/>
+  })
+  return(
     <div>
+      {jokeElements}
+    </div>
+  )
+}
+
+/*
       <Joke 
         setup="I got my daughter a fridge for her birthday" 
         punchline ="I can't wait to see her face light up when she opens it"
@@ -34,8 +50,6 @@ function App() {
       <Joke 
         punchline = "It's hard to explain puns to kleptomaniacs because they always take things literally"
         isPun={true}/>
-    </div>
-  );
-}
+ */
 
 export default App;
---
+++ src/jokesData.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        question:"I got my daughter a fridge for her birthday",
+        answer:"I can't wait to see her face light up when she opens it"
+    },
+    {
+        question:"How did the hacker escape the police?",
+        answer: "He just ransomware" 
+    },
+    {
+        question:"Why don't pirates travel on mountain roads?",
+        answer:"Scurvy"
+    },
+    {
+        question: "Why do bees stay in the hive during winters?",
+        answer:"Swarm"
+    },
+    {   
+        question:"What's the best thing about Switzerland?", 
+        answer:"I don't know, but the flag is a big plus!"
+    }
+]
